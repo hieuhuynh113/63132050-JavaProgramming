7d4ae70d2e9f02be67edd6b24f9eacab7dd4c11f
hieuhuynh113 <hieu01645369512@gmail.com>
2024-01-24 16:59:23 +0700

    Delta

---
+++ Lab4/src/Delta.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Delta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+        
+        double delta = Math.pow(b, 2) - 4 * a * c;
+        double canDelta = Math.sqrt(delta);
+        
+        System.out.println("Căn delta: " + canDelta);
+    }
+}
