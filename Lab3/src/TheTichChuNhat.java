19f56666e2c9fb2fb353aecb681c25677d9c5bdc
hieuhuynh113 <hieu01645369512@gmail.com>
2024-01-24 16:56:49 +0700

    Lab3

---
+++ Lab3/src/TheTichChuNhat.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TheTichChuNhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập cạnh của khối lập phương: ");
+        double canh = scanner.nextDouble();
+        
+        double theTich = Math.pow(canh, 3);
+        
+        System.out.println("Thể tích của khối lập phương: " + theTich);
+    }
+}
