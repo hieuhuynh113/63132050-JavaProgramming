9af368bc8220fcd068ae9447bedc193ce2ed1a55
hieuhuynh113 <hieu01645369512@gmail.com>
2024-02-23 13:13:30 +0700

    Bai1_Lab3

---
+++ Lab3/src/Bai1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập một số nguyên dương: ");
+        int N = scanner.nextInt();
+
+        boolean ok = true;
+        for (int i = 2; i < N - 1; i++) {
+            if (N % i == 0) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok && N > 1) {
+            System.out.println(N + " là số nguyên tố.");
+        } else {
+            System.out.println(N + " không là số nguyên tố.");
+        }
+    }
+}
