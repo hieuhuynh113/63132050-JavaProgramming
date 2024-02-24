d970949a94b102358f673788b042d0ba29958db8
hieuhuynh113 <hieu01645369512@gmail.com>
2024-02-24 10:35:14 +0700

    Create button

--- HelloJFrame/src/ChuongTrinhChinh.java
+++ HelloJFrame/src/ChuongTrinhChinh.java
@@ -1,11 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 import javax.swing.WindowConstants;
+
 public class ChuongTrinhChinh {
 	public static void main (String[] args) {
 		JFrame f = new JFrame (); //Tạo một thể hiện (đối tượng) của lớp 
 		f.setTitle("Ví dụ JFrame"); //Tiêu đề cửa sổ
 		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		f.setSize(300, 200); //Cửa sổ kích thước
+		
+		//Tạo một nút bấm
+		JButton btnOK = new JButton("Nhấn tôi đi");
+		JButton btnKhac = new JButton("Nhấn em đi");
+		//Gắn lên form
+		f.add(btnOK);
+		f.add(btnKhac);
 		f.show(); //Hiện cửa sổ 
 	}
 }
